9abda7eb93919c96bb8f2849f58952e024a97e36
jimin <w2037@e-mirim.hs.kr>
2021-05-21 09:55:39 +0900

    문제5qjs 방법2

--- 문05.py
+++ 문05.py
@@ -1,15 +1,16 @@
 def solution(number):
-   count = 0
-   for i in range(1, number + 1):  # for(int i=1; i<number+1)
-       current = i
-       temp = count
-       while current != 0:
-           if current % 10 == 3 or current % 10 == 6 or current % 10 == 9:
-               count += 1
+    count = 0
+    for i in range(1, number + 1):  # for(int i=1; i<number+1)
+        current = i
+        temp = count
+        while current != 0:
+            if current % 10 % 3 ==0 and current%10 !=0:
+                count += 1
             current = current // 10
-       return count
+        return count
 
-#The following is code to output testcase.
+
+# The following is code to output testcase.
 number = 40
 ret = solution(number)
 print(ret)
