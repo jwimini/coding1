1d83a1ca4e2c90d7fbd5923040c8b3f9ce3c0a8d
jimin <w2037@e-mirim.hs.kr>
2021-06-11 09:13:25 +0900

    Lv1-01 음양더하기 완성

--- 프로그래머스/01 음양더하기.py
+++ 프로그래머스/01 음양더하기.py
@@ -1,5 +1,13 @@
 def solution(absolutes, signs):
-    answer = 123456789
+    answer = 0
+    for i in range(len(absolutes)):
+        # True에 해당하는 요소(item)은 더한다
+        if signs[i] is True:
+            answer += absolutes[i]
+        # False 에 해당하는 요소(item)은 뺀다
+        else:
+            answer -= absolutes[i]
+
     return answer
 
 
